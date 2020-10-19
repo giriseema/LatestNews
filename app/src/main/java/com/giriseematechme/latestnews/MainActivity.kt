1f8633882508036924d5ca6cb03eb685a3eecb6b
Seema <seema@innovanathinklabs.com>
2020-10-19 12:52:19 +0530

    Initial commit

---
+++ app/src/main/java/com/giriseematechme/latestnews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.giriseematechme.latestnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
